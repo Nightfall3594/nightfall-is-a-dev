bfe1dd91a9990793db18ab119640df4e6273e9ac
Nightfall3594 <nightfall3594@gmail.com>
2025-07-21 17:33:21 +0800

    feat(bottom-nav): add leave a message input form

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// Navbar Event Listeners
 const overlay = document.querySelector('.overlay');
 const hamburgerIcon = document.querySelector('.hamburger-menu');
 const mobileNavLinks = document.querySelector('.navbar__links');
@@ -17,3 +18,23 @@ mobileNavLinks.addEventListener('click', () => {
     overlay.classList.remove('visible');
     mobileNavLinks.classList.remove('visible');
 })
+
+
+// Bottom Navigation Event Listeners
+const bottomNavCommentButton = document.querySelector('.bottom-nav__comment');
+const bottomNavCommentInput = document.querySelector('.bottom-nav__leave-message');
+const bottomNavGlobalOverlay = document.querySelector('.bottom-nav__overlay');
+const bottomNavCloseButton = document.querySelector('.bottom-nav-message__close');
+
+bottomNavCommentButton.addEventListener('click', () => {
+    bottomNavGlobalOverlay.classList.add('visible');
+    bottomNavCommentInput.classList.add('visible');
+})
+
+bottomNavCloseButton.addEventListener('click', () => {
+    bottomNavGlobalOverlay.classList.remove('visible');
+    bottomNavCommentInput.classList.remove('visible');
+})
+    
+
+
