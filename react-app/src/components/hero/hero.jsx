65328dbb40bb98fec9fd7f2f9afa92441898a2d9
Nightfall3594 <nightfall3594@gmail.com>
2025-07-25 14:45:42 +0800

    refactor(hero): migrate hero links, hero text, and subtle scroll into react components

--- react-app/src/components/hero/hero.jsx
+++ react-app/src/components/hero/hero.jsx
@@ -1,40 +1,32 @@
 import {Mail, Discord, Github, SubtleScroll} from "../icons/index.js";
+import HeroLink from "./hero-link.jsx";
+import HeroText from "./hero-text.jsx";
 
 function Hero({className=""}) {
     return (
-        <>
-            <section className="hero">
-                <div className="hero__container">
-                    <div className="hero__text-container">
-                        <p>Hi! I'm Nightfall 👋</p>
-                        <p>I'm a <span>&lt;Python Developer/&gt;</span></p>
-                        <p className="hero__text--muted">I like to code because I like building stuff</p>
+        <section className="hero">
+            <div className="hero__container">
+                <div className="hero__text-container">
 
-                        <div className="hero__links">
+                    <HeroText/>
 
-                            <a href="#">
-                                <Github className="hero__icon github-icon" />
-                            </a>
-
-                            <a href="#">
-                                <Mail className="hero__icon mail-icon" />
-                            </a>
-
-                            <a href="#">
-                                <Discord className="hero__icon discord-icon" />
-                            </a>
-                        </div>
+                    <div className="hero__links">
+                        <HeroLink Icon={Github} className="hero__icon github-icon" to="#"/>
+                        <HeroLink Icon={Mail} className="hero__icon mail-icon" to="#"/>
+                        <HeroLink Icon={Discord} className="hero__icon discord-icon" to="#"/>
                     </div>
 
-                    <img src="/images/pfp.jpg" className="hero__image " alt="hero image"/>
                 </div>
 
-                <div className="subtle-scroll">
-                    <p>Come have a look!</p>
-                    <SubtleScroll className='subtle-scroll__icon'/>
-                </div>
-            </section>
-        </>
+                <img src="/images/pfp.jpg" className="hero__image " alt="hero image"/>
+
+            </div>
+
+            <div className="subtle-scroll">
+                <p>Come have a look!</p>
+                <SubtleScroll className='subtle-scroll__icon'/>
+            </div>
+        </section>
     )
 }
 
