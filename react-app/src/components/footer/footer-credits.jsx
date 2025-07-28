727d96fd1e40e8589821c191be6f8fe48b9d5f96
Nightfall3594 <nightfall3594@gmail.com>
2025-07-28 13:51:42 +0800

    refactor(footer): migrate all of footer to react

---
+++ react-app/src/components/footer/footer-credits.jsx
@@ -0,0 +1,17 @@
+
+function FooterCredits(){
+    return (
+        <>
+            <div className="footer__container-2">
+                <p className="footer__copyright"> © 2023-2025 Nightfall</p>
+                <p className="footer__quote">Stay Humble, Learn More.</p>
+            </div>
+
+            <p className="footer__credits">
+                Inspired by Innei's <a href="https://github.com/Innei/Shiro">Mix-Space Shiro </a>
+            </p>
+        </>
+    )
+}
+
+export default FooterCredits;
