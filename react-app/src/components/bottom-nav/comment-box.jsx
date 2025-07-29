1901433ec16717c1ab11376d72a777124bee125a
Nightfall3594 <nightfall3594@gmail.com>
2025-07-29 22:36:17 +0800

    refactor(bottom-nav): add overlay to comment box

--- react-app/src/components/bottom-nav/comment-box.jsx
+++ react-app/src/components/bottom-nav/comment-box.jsx
@@ -1,4 +1,5 @@
 import {CloseIcon} from "../icons/index.js";
+import Overlay from "../overlay.jsx";
 
 function CommentBox({isVisible, onClose}){
 
@@ -7,23 +8,26 @@ function CommentBox({isVisible, onClose}){
     }
 
     return (
-        <div className="bottom-nav__leave-message ">
-            <div className="bottom-nav-title__container">
-                <h1 className="bottom-nav-message__title">Leave a message!</h1>
-                <button className="bottom-nav-message__close" onClick={onClose}>
-                    <CloseIcon className="bottom-nav-close__icon"/>
-                </button>
+        <>
+            <Overlay/>
+            <div className="bottom-nav__leave-message ">
+                <div className="bottom-nav-title__container">
+                    <h1 className="bottom-nav-message__title">Leave a message!</h1>
+                    <button className="bottom-nav-message__close" onClick={onClose}>
+                        <CloseIcon className="bottom-nav-close__icon"/>
+                    </button>
+                </div>
+
+                <hr/>
+
+                <p className="bottom-nav-message__pretext">Let me know your thoughts!</p>
+                <form action="#" method="post">
+                    <textarea name="comment" id="comment" placeholder="What do you think?"></textarea>
+                    <button type="submit" className="bottom-nav-message__send">Send</button>
+                </form>
+
             </div>
-
-            <hr/>
-
-            <p className="bottom-nav-message__pretext">Let me know your thoughts!</p>
-            <form action="#" method="post">
-                <textarea name="comment" id="comment" placeholder="What do you think?"></textarea>
-                <button type="submit" className="bottom-nav-message__send">Send</button>
-            </form>
-
-        </div>
+        </>
     )
 }
 
