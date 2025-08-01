c01bc51a81fc5ccd4b0a00452fc7ddad06620322
Nightfall3594 <nightfall3594@gmail.com>
2025-08-01 22:11:18 +0800

    fix: fixed non-disappearing mobile nav on resize and handled resizing edge cases

--- src/components/common/navbar/navbar.jsx
+++ src/components/common/navbar/navbar.jsx
@@ -5,7 +5,7 @@ import NavLinks from "./navlinks.jsx";
 import * as Icons from '../icons/index.js'
 import Overlay from "../overlays/overlay.jsx";
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 function NavBar() {
@@ -13,25 +13,43 @@ function NavBar() {
     const [isOverlayVisible, setOverlayVisible] = useState(false);
     const [isNavLinkVisible, setNavLinkVisible] = useState(false);
 
+    let hideNav = () => {
+        setOverlayVisible(false);
+        setNavLinkVisible(false);
+    }
+
+    let showNav = () => {
+        setOverlayVisible(true);
+        setNavLinkVisible(true);
+    }
+
+    /* These functions are for responsive behavior on the JS side of things. */
+    let handleResize = () =>
+    {
+        if (window.innerWidth > 768) {
+            hideNav();
+        }
+    }
+
+    useEffect( () =>
+    {
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    })
+
+
     return (
         <>
             <Overlay
                 className="navbar__overlay"
-                 onClick={() =>
-                 {
-                     setOverlayVisible(false);
-                     setNavLinkVisible(false);
-                 }}
+                 onClick={hideNav}
                  isVisible={isOverlayVisible}
             />
 
             <header className="navbar">
                 <button
                     className="navbar__icon hamburger-menu"
-                    onClick={() => {
-                        setOverlayVisible(true);
-                        setNavLinkVisible(true)}
-                    }
+                    onClick={showNav}
                 >
                     <Icons.Hamburger className="navbar__icon"/>
                 </button>
@@ -39,11 +57,11 @@ function NavBar() {
                 <img src="/images/pfp.jpg" alt="profile picture" className="navbar__profile-image"/>
 
                 <NavLinks isVisible={isNavLinkVisible}>
-                    <NavItem to="/" Icon={Icons.Home}>Home</NavItem>
-                    <NavItem to="/journal" Icon={Icons.Journal}>Journal</NavItem>
-                    <NavItem to="/thoughts" Icon={Icons.Thought}>Thoughts</NavItem>
-                    <NavItem to="/projects" Icon={Icons.Project}>Projects</NavItem>
-                    <NavItem to="/other" Icon={Icons.Other}>Other</NavItem>
+                    <NavItem to="/" Icon={Icons.Home} onClick={hideNav}>Home</NavItem>
+                    <NavItem to="/journal" Icon={Icons.Journal} onClick={hideNav}>Journal</NavItem>
+                    <NavItem to="/thoughts" Icon={Icons.Thought} onClick={hideNav}>Thoughts</NavItem>
+                    <NavItem to="/projects" Icon={Icons.Project} onClick={hideNav}>Projects</NavItem>
+                    <NavItem to="/other" Icon={Icons.Other} onClick={hideNav}>Other</NavItem>
                 </NavLinks>
 
                 <button className="navbar__icon contact-button">
