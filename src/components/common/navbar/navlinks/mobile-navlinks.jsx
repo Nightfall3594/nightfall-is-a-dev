46cd88c71fc1279882fa6988f1815479c922787a
Nightfall3594 <nightfall3594@gmail.com>
2025-08-11 12:52:11 +0800

    refactor(navbar): clean up mobile nav logic

--- src/components/common/navbar/navlinks/mobile-navlinks.jsx
+++ src/components/common/navbar/navlinks/mobile-navlinks.jsx
@@ -1,28 +1,21 @@
 import {AnimatePresence, easeOut, motion} from "framer-motion";
 import React from "react";
 
-function MobileNavLinks({isVisible, children}) {
 
-    const mobileNavLinkVariants = {
-        hidden: {opacity: 0, y: "100%"},
-        show: {
-            y: "0%",
-            opacity: 1,
-            transition: {
-                staggerChildren: 0.1,
-                delayChildren: 0.2,
-                animation: easeOut
-            }
-        },
-        exit: {
-            y: "100%",
-            opacity: 0,
-            transition: {
-                staggerChildren: 0.05,
-                staggerDirection: -1
-            }
+const mobileNavLinkVariants = {
+    hidden: {opacity: 0, y: "100%"},
+    show: {
+        y: "0%",
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+            delayChildren: 0.2,
+            animation: easeOut
         }
-    };
+    }
+};
+
+function MobileNavLinks({isVisible, children}) {
 
     return (
         <AnimatePresence>
@@ -31,7 +24,7 @@ function MobileNavLinks({isVisible, children}) {
                     className="navbar__links"
                     initial="hidden"
                     animate="show"
-                    exit="exit"
+                    exit="hidden"
                     variants={mobileNavLinkVariants}
                 >
                     <motion.ul>
