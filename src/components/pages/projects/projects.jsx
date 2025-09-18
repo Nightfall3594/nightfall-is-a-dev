842af3ba08d3c4c54fd070dd36fb5e1c399df0c8
Nightfall3594 <nightfall3594@gmail.com>
2025-09-18 09:28:44 +0800

    feat(projects-page): add initial page header

---
+++ src/components/pages/projects/projects.jsx
@@ -0,0 +1,13 @@
+import './projects.css'
+
+export default function Projects(){
+    return (
+        <section className="projects">
+
+            <div className="projects__header">
+                <h1 className="projects__title">Projects</h1>
+                <h2 className="projects__subtitle">Software, tinkering, and everything in between</h2>
+            </div>
+        </section>
+    )
+}
