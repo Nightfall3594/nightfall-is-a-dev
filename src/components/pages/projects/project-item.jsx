1e66a277c39632e964faf44348baff40282e4ad7
Nightfall3594 <nightfall3594@gmail.com>
2025-09-20 19:24:14 +0800

    feat(projects-page): add projects list and project-item component

---
+++ src/components/pages/projects/project-item.jsx
@@ -0,0 +1,16 @@
+import './project-item.css'
+export default function ProjectItem({name, desc, image}) {
+    return (
+        <div className="project-item">
+
+            {image
+                ? <img src={image} alt="project logo" className="project__image"/>
+                : <div className="project__image--empty">{name[0]}</div>
+            }
+            <div className="project-info">
+                <p className="project-info__name">{name}</p>
+                <p className="project-info__desc">{desc}</p>
+            </div>
+        </div>
+    )
+}
--- src/components/pages/projects/projects.jsx
+++ src/components/pages/projects/projects.jsx
@@ -1,4 +1,5 @@
 import './projects.css'
+import ProjectItem from "./project-item.jsx";
 
 export default function Projects(){
     return (
@@ -7,6 +8,14 @@ export default function Projects(){
             <div className="projects__header">
                 <h1 className="projects__title">Projects</h1>
                 <h2 className="projects__subtitle">Software, tinkering, and everything in between</h2>
+            </div>
+
+            <div className="projects__list">
+                <ProjectItem desc="Hello world app writted in python" name={"Hello world"} image={"/images/pfp.jpg"}/>
+                <ProjectItem desc="Simple" name={"Hello world"} image={"/images/pfp.jpg"}/>
+                <ProjectItem desc="Hello world app writted in python and coded in java maybe idk this is filler" name={"Hello world"} />
+                <ProjectItem desc="Hello world app writted in python uvuvwevwevwe onyentenyevew umugubmwem osas" name={"Wello world"} />
+                <ProjectItem desc="Hello world app writted in python" name={"Trello world"} />
             </div>
         </section>
     )
