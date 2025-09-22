07763730f22991369e1e2eca97033613c6179c4c
Nightfall3594 <nightfall3594@gmail.com>
2025-09-22 21:08:16 +0800

    feat(projects-page): add initial animations for projects page

--- src/components/pages/projects/project-item.jsx
+++ src/components/pages/projects/project-item.jsx
@@ -2,6 +2,20 @@ import './project-item.css'
 
 import {motion} from "framer-motion";
 
+const variants = {
+    hidden: {
+        opacity: 0,
+        y: "20%"
+    },
+    visible: {
+        opacity: 1,
+        y: "0%",
+        transition: {
+            animation: "easeInOut"
+        }
+    }
+}
+
 export default function ProjectItem({name, desc, image, link}) {
     return (
         <motion.a
@@ -9,6 +23,7 @@ export default function ProjectItem({name, desc, image, link}) {
             whileHover={{ scale: 1.1 }}
             href={link}
             target="_blank"
+            variants={variants}
         >
 
             {image
--- src/components/pages/projects/projects.jsx
+++ src/components/pages/projects/projects.jsx
@@ -1,22 +1,71 @@
 import './projects.css'
 import ProjectItem from "./project-item.jsx";
+import {motion} from "framer-motion";
+
+const parentVariants = {
+    hidden: {
+        opacity: 0,
+        y: 20
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            staggerChildren: 0.15,
+            animation: "easeInOut"
+        }
+    },
+}
+
+const childVariants = {
+    hidden: {
+        opacity: 0,
+        y: 20
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+    }
+}
 
 export default function Projects(){
     return (
-        <section className="projects">
+        <motion.section
+            className="projects"
+            variants={parentVariants}
+            initial="hidden"
+            animate="visible"
+        >
 
-            <div className="projects__header">
-                <h1 className="projects__title">Projects</h1>
-                <h2 className="projects__subtitle">Software, tinkering, and everything in between</h2>
-            </div>
+            <motion.div
+                className="projects__header"
+                variants={parentVariants}
+            >
+                <motion.h1
+                    className="projects__title"
+                    variants={childVariants}
+                >
+                    Projects
+                </motion.h1>
 
-            <div className="projects__list">
+                <motion.h2
+                    className="projects__subtitle"
+                    variants={childVariants}
+                >
+                    Software, tinkering, and everything in between
+                </motion.h2>
+            </motion.div>
+
+            <motion.div
+                className="projects__list"
+                variants={parentVariants}
+            >
                 <ProjectItem desc="Hello world app writted in python" name={"Hello world"} image={"/images/pfp.jpg"} link={"https://google.com"}/>
                 <ProjectItem desc="Simple" name={"Hello world"} image={"/images/pfp.jpg"} link={"https://google.com"}/>
                 <ProjectItem desc="Hello world app writted in python and coded in java maybe idk this is filler" name={"Hello world"}  link={"https://google.com"}/>
                 <ProjectItem desc="Hello world app writted in python uvuvwevwevwe" name={"Wello world"}  link={"https://google.com"}/>
                 <ProjectItem desc="Hello world app writted in python" name={"Trello world"}  link={"https://google.com"}/>
-            </div>
-        </section>
+            </motion.div>
+        </motion.section>
     )
 }
