0948b92acb984c572e62672e503c895b45eb34d5
Nightfall3594 <nightfall3594@gmail.com>
2025-09-28 21:50:10 +0800

    feat(others-page): add page content and markup

---
+++ src/components/pages/other/other.jsx
@@ -0,0 +1,46 @@
+import './other.css'
+
+export default function Other() {
+    return (
+        <section className="other">
+
+            <div className="other-header__container">
+                <h1 className="other-header__title">About</h1>
+                <h2 className="other-header__subtitle">Information about the little why's and how's</h2>
+            </div>
+
+            <h2 className="about__title">About the site</h2>
+
+            <p className={"about__content"}>
+                Welcome to my webpage! This is my little corner of the internet, where I share projects, thoughts, ideas, and many other random stuff.
+
+                This project originally began as a recreation of an existing design of a similar blog site, so this design isn't fully mine,
+                however, the process provided me with a great refresher on html and css, and over time,
+                I also learned how to use React and related libraries. <br/><br/>
+
+                In the process, I learned a lot about designing and developing web frontends. It taught me naming conventions, file structures,
+                and plenty of the quirks this weird little thing called Javascript. I also learned about responsive design, and many other web-development concepts, such as object destructuring, component-driven-design, constants, and many, many more.
+                <br/><br/>
+
+                So while the website is just a recreation of an existing design, in the process of reverse-engineering, debugging,
+                and analyzing components, I learned a lot, I grew from it, and most importantly, I gained valuable knowledge of which will be useful when newer, more complex projects come later down the line.
+                <br/><br/>
+
+                So, what's going to happen to the website?
+                <br/><br/>
+
+                Personally, I think I'll hold on to it for a while longer. I'll maintain it, share my stories, write random things, and while it isn't anything quite substantial yet,
+                I'd like for this website to be my tiny little space in the internet, for a while. Where I will share my thoughts, my day-to-day stories, projects and journey towards growing and improving constantly as a software developer.
+                <br/><br/>
+
+                So, if you will chime in, read a little, and browse,
+                <br/><br/>
+
+                Welcome to my personal corner of the internet! <br/>
+                Enjoy your stay :)
+
+
+            </p>
+        </section>
+    )
+}
