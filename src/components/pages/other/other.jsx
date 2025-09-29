76dcba677de08393da8126e68693f0953f2fcde8
Nightfall3594 <nightfall3594@gmail.com>
2025-09-29 23:34:54 +0800

    refactor(others-page): split body content into several <p> tags

--- src/components/pages/other/other.jsx
+++ src/components/pages/other/other.jsx
@@ -11,37 +11,46 @@ export default function Other() {
 
             <h2 className="about__title">About the site</h2>
 
-            <p className={"about__content"}>
-                Welcome to my webpage! This is my little corner of the internet, where I share projects, thoughts, ideas, and many other random stuff.
+            <div className={"about__content"}>
+                <p>
+                    Welcome to my webpage! This is my little corner of the internet, where I share projects, thoughts, ideas, and many other random stuff.
+                </p>
 
-                This project originally began as a recreation of an existing design of a similar blog site, so this design isn't fully mine,
-                however, the process provided me with a great refresher on html and css, and over time,
-                I also learned how to use React and related libraries. <br/><br/>
+                <p>
+                    This project originally began as a recreation of an existing design of a similar blog site, so this design isn't fully mine,
+                    however, the process provided me with a great refresher on html and css, and over time,
+                </p>
 
-                In the process, I learned a lot about designing and developing web frontends. It taught me naming conventions, file structures,
-                and plenty of the quirks this weird little thing called Javascript. I also learned about responsive design, and many other web-development concepts, such as object destructuring, component-driven-design, constants, and many, many more.
-                <br/><br/>
+                <p>
+                    In the process, I learned a lot about designing and developing web frontends. It taught me naming conventions, file structures,
+                    and plenty of the quirks this weird little thing called Javascript. I also learned about responsive design, and many other web-development concepts, such as object destructuring, component-driven-design, constants, and many, many more.
+                    So while the website is just a recreation of an existing design, in the process of reverse-engineering, debugging,
+                    and analyzing components, I learned a lot, I grew from it, and most importantly, I gained valuable knowledge of which will be useful when newer, more complex projects come later down the line.
+                </p>
 
-                So while the website is just a recreation of an existing design, in the process of reverse-engineering, debugging,
-                and analyzing components, I learned a lot, I grew from it, and most importantly, I gained valuable knowledge of which will be useful when newer, more complex projects come later down the line.
-                <br/><br/>
+                <p>
+                    So, what's going to happen to the website?
+                </p>
 
-                So, what's going to happen to the website?
-                <br/><br/>
+                <p>
+                    Personally, I think I'll hold on to it for a while longer. I'll maintain it, share my stories, write random things, and while it isn't anything quite substantial yet,
+                    I'd like for this website to be my tiny little space in the internet, for a while. Where I will share my thoughts, my day-to-day stories, projects and journey towards growing and improving constantly as a software developer.
+                </p>
 
-                Personally, I think I'll hold on to it for a while longer. I'll maintain it, share my stories, write random things, and while it isn't anything quite substantial yet,
-                I'd like for this website to be my tiny little space in the internet, for a while. Where I will share my thoughts, my day-to-day stories, projects and journey towards growing and improving constantly as a software developer.
-                <br/><br/>
+                <p>
+                    So, if you will chime in, read a little, and browse,
+                </p>
 
-                So, if you will chime in, read a little, and browse,
-                <br/><br/>
+                <p>
+                    Welcome to my personal corner of the internet!
+                </p>
 
-                Welcome to my personal corner of the internet!
-                <br/><br/>
-                Enjoy your stay :)
+                <p>
+                    Enjoy your stay :)
+                </p>
 
 
-            </p>
+            </div>
         </section>
     )
 }
