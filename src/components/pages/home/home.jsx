f40584d764d09af271a7236ed2d52dfb5d4a2b31
Nightfall3594 <nightfall3594@gmail.com>
2025-10-24 23:42:17 +0800

    feat(home-page): link home page to api

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -2,11 +2,23 @@ import Hero from "./hero/hero.jsx";
 import Timelines from "./timelines/timelines.jsx";
 import BottomNav from "./bottom-nav/bottom-nav.jsx";
 
+import useHomePageData from "../../../hooks/useHomePageData.js";
+import LoadingScreen from "../../common/loading-screen/loading-screen.jsx";
+
 function Home(){
+
+    const {isLoading, recentNotes, recentProjects, timeline} = useHomePageData();
+
+    if(isLoading) return <LoadingScreen/>
+
     return(
         <>
             <Hero />
-            <Timelines />
+            <Timelines
+                recentNotes={recentNotes}
+                recentProjects={recentProjects}
+                timeline={timeline}
+            />
             <BottomNav />
         </>
     )
