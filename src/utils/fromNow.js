b07712f8279a21ac16f69cd48adb98c08adeb990
Nightfall3594 <nightfall3594@gmail.com>
2025-11-09 18:49:17 +0800

    fix: fix inaccurate date error on article post page

--- src/utils/fromNow.js
+++ src/utils/fromNow.js
@@ -1,8 +1,12 @@
 import dayjs from 'dayjs'
 import relativeTime from "dayjs/plugin/relativeTime";
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export default function fromNow(date) {
-    return dayjs(date).fromNow();
+    return dayjs.utc(date).tz("America/New_York").fromNow();
 }
