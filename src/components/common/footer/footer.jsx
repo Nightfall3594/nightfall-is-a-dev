e6b92812a9cb77a314a9e03ba749eab8382d4057
Nightfall3594 <nightfall3594@gmail.com>
2025-11-09 18:08:16 +0800

    fix: fix swapped link issue in footer
    how i missed that is beyond me. Was i sleeping when i coded it?

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
     return (
         <section className="footer">
             <FooterContacts>
-                <FooterContact to="https://github.com/Nightfall3594" content="Email"/>
-                <FooterContact to="mailto:nightfall3594@gmail.com" content="Github"/>
+                <FooterContact to="mailto:nightfall3594@gmail.com" content="Email"/>
+                <FooterContact to="https://github.com/Nightfall3594" content="Github"/>
                 <FooterContact to="https://discord.com/users/335641970170920960" content="Discord"/>
             </FooterContacts>
 
