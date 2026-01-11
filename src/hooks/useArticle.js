6242a4fcd66a58a9274d30b9a60778325e150233
Nightfall3594 <briankristoferperez28@gmail.com>
2026-01-11 22:20:32 +0800

    feat(article-post): add error handling page

--- src/hooks/useArticle.js
+++ src/hooks/useArticle.js
@@ -5,16 +5,32 @@ export default function useArticle(article_slug){
 
     const [article, setArticle] = useState(null);
     const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch(`${API_URL}/articles/${article_slug}`)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    if (res.status === 404) {
+                        throw new Error("Oops! Article not found ");
+                    } else if (res.status === 500) {
+                        throw new Error("Something went wrong on our end ");
+                    } else {
+                        throw new Error(`Unexpected error: ${res.status}`);
+                    }
+                }
+                return res.json();
+            })
             .then(data => {
                 setArticle(data);
                 setLoading(false);
             })
+            .catch(err => {
+                setError(err.message)
+                setLoading(false);
+            })
     },[article_slug])
 
-    return {article, isLoading}
+    return {article, isLoading, error}
 
 }
